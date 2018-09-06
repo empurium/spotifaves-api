eef53479676aa26cece8342c8aec916076ff697e
Michael Simmons <github@e7i.org>
2018-09-06 21:48:47 +0800

    feat(Users): Users table in Sequelize, migration included

---
+++ migrations/20180906133753-create-user.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER.UNSIGNED,
+      },
+      first_name: {
+        type: Sequelize.STRING,
+      },
+      last_name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      password_reset_token: {
+        type: Sequelize.STRING,
+      },
+      password_reset_expires: {
+        type: Sequelize.STRING,
+      },
+      photo_url: {
+        type: Sequelize.STRING,
+      },
+      tokens: {
+        type: Sequelize.JSON,
+      },
+      spotify: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users'),
+};
