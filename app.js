bf840e2e9606757d1f2fa54b67324a32ba8c7800
Michael Simmons <github@e7i.org>
2018-09-07 01:27:50 +0800

    refactor(app): Going API-only

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 // require('newrelic');
 const express = require('express');
 const compression = require('compression');
-const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const logger = require('morgan');
 const chalk = require('chalk');
 const errorHandler = require('errorhandler');
 const lusca = require('lusca');
 const dotenv = require('dotenv');
-const flash = require('express-flash');
-const path = require('path');
 const passport = require('passport');
 const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
-const sass = require('node-sass-middleware');
-const multer = require('multer');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 /**
- * Load environment variables from .env file, where API keys and passwords are configured.
+ * Load environment variables from .env file.
  */
 dotenv.load();
 
@@ -36,43 +32,23 @@ const sessionStore = new SequelizeStore({ db: db.sequelize });
 sessionStore.sync();
 
 /**
- * Multer uploader.
- */
-const upload = multer({ dest: path.join(__dirname, 'uploads') });
-
-/**
  * Controllers (route handlers).
  */
-const homeController = require('./controllers/home');
-const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
-const contactController = require('./controllers/contact');
 
 /**
- * API keys and Passport configuration.
+ * Passport authentication configuration.
  */
-const passportConfig = require('./config/passport');
+require('./config/passport');
 
 /**
- * Create Express server.
+ * Express!
  */
 const app = express();
-
-/**
- * Express configuration.
- */
 app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 app.use(expressStatusMonitor());
 app.use(compression());
-app.use(
-  sass({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-  }),
-);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(expressValidator());
@@ -88,14 +64,6 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(flash());
-app.use((req, res, next) => {
-  if (req.path === '/api/upload' || req.path === '/signup') {
-    next();
-  } else {
-    lusca.csrf()(req, res, next);
-  }
-});
 app.use(lusca.xframe('SAMEORIGIN'));
 app.use(lusca.xssProtection(true));
 app.disable('x-powered-by');
@@ -103,80 +71,12 @@ app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
 });
-app.use((req, res, next) => {
-  // After successful login, redirect back to the intended page
-  if (
-    !req.user &&
-    req.path !== '/login' &&
-    req.path !== '/signup' &&
-    !req.path.match(/^\/auth/) &&
-    !req.path.match(/\./)
-  ) {
-    req.session.returnTo = req.originalUrl;
-  } else if (req.user && (req.path === '/account' || req.path.match(/^\/api/))) {
-    req.session.returnTo = req.originalUrl;
-  }
-  next();
-});
-app.use('/', express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
-app.use(
-  '/js/lib',
-  express.static(path.join(__dirname, 'node_modules/popper.js/dist/umd'), { maxAge: 31557600000 }),
-);
-app.use(
-  '/js/lib',
-  express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'), { maxAge: 31557600000 }),
-);
-app.use(
-  '/js/lib',
-  express.static(path.join(__dirname, 'node_modules/jquery/dist'), { maxAge: 31557600000 }),
-);
-app.use(
-  '/webfonts',
-  express.static(path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free/webfonts'), {
-    maxAge: 31557600000,
-  }),
-);
 
 /**
- * Primary app routes.
+ * API routes.
  */
-app.get('/', homeController.index);
-app.get('/login', userController.getLogin);
-app.post('/login', userController.postLogin);
-app.get('/logout', userController.logout);
-app.get('/forgot', userController.getForgot);
-app.post('/forgot', userController.postForgot);
-app.get('/reset/:token', userController.getReset);
-app.post('/reset/:token', userController.postReset);
-app.get('/signup', userController.getSignup);
-app.post('/signup', userController.postSignup);
-app.get('/contact', contactController.getContact);
-app.post('/contact', contactController.postContact);
-app.get('/account', passportConfig.isAuthenticated, userController.getAccount);
-app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
-app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
-app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
-app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
-
-/**
- * API examples routes.
- */
-app.get('/api', apiController.getApi);
+app.get('/', apiController.getIndex);
 app.get('/api/artists', apiController.getArtists);
-app.get('/api/lastfm', apiController.getLastfm);
-app.get('/api/stripe', apiController.getStripe);
-app.post('/api/stripe', apiController.postStripe);
-app.get('/api/clockwork', apiController.getClockwork);
-app.post('/api/clockwork', apiController.postClockwork);
-app.get(
-  '/api/facebook',
-  passportConfig.isAuthenticated,
-  passportConfig.isAuthorized,
-  apiController.getFacebook,
-);
-app.get('/api/upload', apiController.getFileUpload);
-app.post('/api/upload', upload.single('myFile'), apiController.postFileUpload);
 
 /**
  * OAuth authentication routes. (Sign in)
@@ -194,10 +94,9 @@ app.get(
 );
 
 /**
- * Error Handler.
+ * Error Handler. Only for development, shows
  */
 if (process.env.NODE_ENV === 'development') {
-  // only use in development
   app.use(errorHandler());
 }
 
