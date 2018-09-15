6e112ddd7e6dd0e414d5f2626aa416ea63dc7021
Michael Simmons <github@e7i.org>
2018-09-15 13:07:30 +0800

    refactor(db): Create tables with charset utf8, rerun migrations

--- migrations/20180906132000-create-artist.js
+++ migrations/20180906132000-create-artist.js
@@ -1,26 +1,33 @@
 module.exports = {
   up: (queryInterface, Sequelize) =>
-    queryInterface.createTable('Artists', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    queryInterface.createTable(
+      'Artists',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        name: {
+          type: Sequelize.STRING,
+        },
+        albums: {
+          type: Sequelize.INTEGER,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      name: {
-        type: Sequelize.STRING,
+      {
+        engine: 'InnoDB',
+        charset: 'utf8',
       },
-      albums: {
-        type: Sequelize.INTEGER,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    }),
+    ),
   down: (queryInterface, Sequelize) => queryInterface.dropTable('Artists'),
 };
--- migrations/20180906133753-create-user.js
+++ migrations/20180906133753-create-user.js
@@ -1,47 +1,54 @@
 module.exports = {
   up: (queryInterface, Sequelize) =>
-    queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER.UNSIGNED,
+    queryInterface.createTable(
+      'Users',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER.UNSIGNED,
+        },
+        first_name: {
+          type: Sequelize.STRING,
+        },
+        last_name: {
+          type: Sequelize.STRING,
+        },
+        email: {
+          type: Sequelize.STRING,
+        },
+        password: {
+          type: Sequelize.STRING,
+        },
+        password_reset_token: {
+          type: Sequelize.STRING,
+        },
+        password_reset_expires: {
+          type: Sequelize.STRING,
+        },
+        photo_url: {
+          type: Sequelize.STRING,
+        },
+        tokens: {
+          type: Sequelize.JSON,
+        },
+        spotify: {
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      first_name: {
-        type: Sequelize.STRING,
+      {
+        engine: 'InnoDB',
+        charset: 'utf8',
       },
-      last_name: {
-        type: Sequelize.STRING,
-      },
-      email: {
-        type: Sequelize.STRING,
-      },
-      password: {
-        type: Sequelize.STRING,
-      },
-      password_reset_token: {
-        type: Sequelize.STRING,
-      },
-      password_reset_expires: {
-        type: Sequelize.STRING,
-      },
-      photo_url: {
-        type: Sequelize.STRING,
-      },
-      tokens: {
-        type: Sequelize.JSON,
-      },
-      spotify: {
-        type: Sequelize.STRING,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    }),
+    ),
   down: (queryInterface, Sequelize) => queryInterface.dropTable('Users'),
 };
