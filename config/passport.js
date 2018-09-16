0bbd80d0e679ada4de115f0f468104d0beee0ca6
Michael Simmons <github@e7i.org>
2018-09-16 16:39:36 +0800

    feat(auth): Update access/refresh tokens upon Spotify authentication

--- config/passport.js
+++ config/passport.js
@@ -35,35 +35,47 @@ passport.use(
       callbackURL: process.env.SPOTIFY_REDIRECT_URI,
     },
     (accessToken, refreshToken, expiresIn, profile, done) => {
-      let email = '';
-      let photoUrl = '';
-
       // Pull first email from the array
+      let email = '';
       if (Array.isArray(profile.emails) && profile.emails.length) {
         email = profile.emails.shift();
         email = email.value;
       }
 
       // Pull first photo from the array
+      let photoUrl = '';
       if (Array.isArray(profile.photos) && profile.photos.length) {
         photoUrl = profile.photos.shift();
       }
 
-      // Create the user if we don't have this spotify_id
-      models.User.findOrCreate({
-        where: { spotify_id: profile.id },
-        defaults: {
-          name: profile.displayName,
-          email,
-          photo_url: photoUrl,
-          spotify_id: profile.id,
-          tokens: {
-            access_token: accessToken,
-            refresh_token: refreshToken,
-            expires_in: expiresIn,
-          },
+      // Our user
+      const userData = {
+        email,
+        name: profile.displayName,
+        photo_url: photoUrl,
+        spotify_id: profile.id,
+        tokens: {
+          access_token: accessToken,
+          refresh_token: refreshToken,
+          expires_in: expiresIn,
         },
-      }).spread((user) => done(false, user.get({ plain: true })));
+      };
+
+      // Create the user if we don't have this spotify_id
+      models.User.findOrCreate({ where: { spotify_id: profile.id }, defaults: userData })
+        .spread((user, created) => {
+          if (created) {
+            done(false, user.get({ plain: true }));
+            return;
+          }
+
+          // Update the tokens and other data for the existing user
+          return user
+            .update(userData)
+            .then((user) => done(false, user.get({ plain: true })))
+            .catch(() => done(false, { id: 0 }));
+        })
+        .catch(() => done(false, { id: 0 }));
     },
   ),
 );
